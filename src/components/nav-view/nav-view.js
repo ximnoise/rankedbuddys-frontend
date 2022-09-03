fd810849942f300e6a01e611e76cb9c3f1b47dd3
ximnoise <mrogge96@protomail.com>
2022-09-03 10:00:50 +0200

    Add Bootstrap / Add NavView / Change Folders

---
+++ src/components/nav-view/nav-view.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import './nav-view.css';
+
+function NavView() {
+	return (
+		<nav class="navbar navbar-expand-lg bg-light">
+			<div class="container-fluid">
+				<Link className="navbar-brand" to={'/'}>
+					RANKEDBUDDYS
+				</Link>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNavAltMarkup"
+					aria-controls="navbarNavAltMarkup"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div class="navbar-nav">
+						<Link className="nav-link" to={'/'}>
+							/ HOME
+						</Link>
+						<Link className="nav-link" to={'/'}>
+							/ DUO
+						</Link>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+}
+
+export default NavView;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Main from './components/main-view/main-view';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Main />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
